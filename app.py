ab9f6220e4ff50d71df361e80e725768f817491c
Gmsantos2 <gmsantos2@utpl.edu.ec>
2022-08-03 00:41:03 -0500

    fix and document app

--- app.py
+++ app.py
@@ -12,27 +12,20 @@
 # pip freeze
 # flask run
 
-from html import entities
-import matplotlib.pyplot as plt
+
 from pprint import pprint
 from SPARQLWrapper import SPARQLWrapper, JSON, N3
-from rdflib import Graph
-import time
-import random
-from io import BytesIO
-from wordcloud import WordCloud
+
+# flask
 from flask import Flask, url_for, request, render_template, jsonify, send_file
 from flask_bootstrap import Bootstrap
 import json
 
 # NLP Pkgs
 import spacy
-from textblob import TextBlob
 nlp = spacy.load("es_core_news_sm")
 # nlp = spacy.load("en_core_web_sm")
 
-
-# WordCloud & Matplotlib Packages
 
 
 # Initialize App
@@ -40,43 +33,46 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
+#ruta inicial donde nos devuelve la página principal
 @app.route('/')
 def index():
 	return render_template('index.html')
 
 
+#ruta de analisis del texto enviado por el metodo post
 @app.route('/analyze', methods=['GET', 'POST'])
+
+#funcion que me devuelve el template de mi página con algunos atributos a presetar (tokens, entidades, resultados de la consulta a dbpedia)
 def analyze():
-	start = time.time()
-	# Receives the input query from form
 	
 
 	if request.method == 'POST':
 		rawtext = request.form['rawtext']
 		# Analysis
-
 		docx = nlp(rawtext)
-		for ent in docx.ents:
-			if ent.label_ in ['LOC', 'GPE']:
-				print(ent.text, ent.label_)
 
 		# Tokens
 		custom_tokens = [token.text for token in docx]
 		# NER
-		# custom_namedentities = []
+
+		#entidades
 		custom_namedentities = [(entity.text, entity.label_)for entity in docx.ents]
 		entitieslabel = [(entity.text)for entity in docx.ents]
-		# for ent in docx.ents:
 
-		# print(custom_namedentities)
-		# print("arriba")
 
+		#especificacion hacia que servicio de datos nos vamos a comunicar (dbpedia sparql) para hacer la consulta 
 		sparql = SPARQLWrapper('https://dbpedia.org/sparql')
+		
+		#print de prueba
 		print(entitieslabel[0])
 		val = entitieslabel[0]
 
 		val = val.replace (' ', '_')
+		
+		#print de prueba
 		print(val)
+
+		#consulta a dbpedia desde dbpedia sparql 
 		sparql.setQuery(f'''
     		SELECT ?object ?image
     		WHERE {{ dbr:{val} rdfs:label ?object .
@@ -85,6 +81,7 @@ def analyze():
 		sparql.setReturnFormat(JSON)
 		qres = sparql.query().convert()
 		
+		#print de prueba
 		print(qres)
 		for result in qres['results']['bindings']:
     	# print(result['object'])
@@ -96,20 +93,16 @@ def analyze():
 			
 			print(f'Lang: {lang}\tValue: {value}\Image: {imag}')
 
-			
+		#obtencion de valores resultantes de la consulta	
 		result_img = [(result['image']['value'])for result in qres['results']['bindings']]
 		result_lan = [(result['object']['xml:lang'], result['object']['value'])for result in qres['results']['bindings']]
 		
-		blob = TextBlob(rawtext)
-		blob_sentiment, blob_subjectivity = blob.sentiment.polarity, blob.sentiment.subjectivity
+		#información de la palabra que nos brinda el componente del modelo de spacy
 		allData = [('"Token":"{}","Tag":"{}","POS":"{}","Dependency":"{}","Lemma":"{}","Shape":"{}","Alpha":"{}","IsStopword":"{}"'.format(
 		    token.text, token.tag_, token.pos_, token.dep_, token.lemma_, token.shape_, token.is_alpha, token.is_stop)) for token in docx]
 
 		result_json = json.dumps(allData, sort_keys=False, indent=2)
 
-		end = time.time()
-		final_time = end-start
-	
 	
 	return render_template('index.html',ctext=rawtext,custom_tokens=custom_tokens,custom_namedentities=custom_namedentities,result_json=result_json,result_lan=result_lan, result_img= result_img)
 
